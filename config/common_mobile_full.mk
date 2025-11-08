c1cd2c403eed502d66e094635ffc25e5b1af9fa0
limoniumstatice <strangeholic@gmail.com>
2025-11-08 21:13:14 +0900

    mod2
    Change-Id: I3b3c85adae344b79330c41c5825fe55bb0571a94

--- config/common_mobile_full.mk
+++ config/common_mobile_full.mk
@@ -10,10 +10,8 @@ $(call inherit-product-if-exists, external/google-fonts/rubik/fonts.mk)
 # Apps
 PRODUCT_PACKAGES += \
     Camelot \
-    Etar \
     Profiles \
     Recorder \
-    Seedvault \
     Twelve
 
 ifneq ($(PRODUCT_NO_CAMERA),true)
